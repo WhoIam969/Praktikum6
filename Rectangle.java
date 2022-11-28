81cc3181d00fbdcface834f35cd9c5ffcdb99328
WhoIam969 <rizkiberkah107@gmail.com>
2022-11-28 11:09:53 +0700

    Program

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+package Pertemuan_10;
+
+public class Rectangle extends Shape {
+    private float height;
+    private float width;
+    public Rectangle(float width, float height) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+    
+    @Override
+    public float getAreas() {
+        return this.width * this.height;
+    }
+}
+    
